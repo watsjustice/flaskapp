e3be498668e401c2a5c6e99c47fc24faf38c155e
watsjustice <88859162+watsjustice@users.noreply.github.com>
2021-11-10 19:23:10 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,41 +1,37 @@
 import flask
 import re
-import random
 import requests
-
-
+from flask_login import logout_user
 
 users = {}
-
-def check(flag):
-
-	if flag:
-		flask.session.clear() 
-	else:
-		pass
-
-
 app = flask.Flask('my-server')
+
+
 
 @app.route('/inf' , methods = ['GET'])
 def qwe():
-    return users
-    
+	return users
+
+
+
+
 @app.route('/inf' , methods = ['POST'])
 def qs():
-    global users
+	global users
 
-    check(flag)
+	try:
+		if flask.request.form['Access'] == 'Allowed':
+			users = {}
+	except:
+		pass
 
-
-    users[flask.request.form['id']] = {
-
-		'name' :  re.sub( '/n|1|2|3|4|5|6|7|8|9|,' , '' , str(flask.request.form['name'])) ,
-		'email' : flask.request.form['email'] , 
-		'status' : flask.request.form['status']
-
-	}
-    
+	users[flask.request.form['id']] = {
+			'name' :  re.sub( '/n|1|2|3|4|5|6|7|8|9|,' , '' , str(flask.request.form['name'])) ,
+			'email' : flask.request.form['email'] , 
+			'status' : flask.request.form['status']
+			}
+			
+	return users
 
 
 
